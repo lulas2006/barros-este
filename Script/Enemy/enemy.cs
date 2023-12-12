40786609f491cd433d006bf5e2c737344f5182b2
António Barros <abotinasbarros@gmail.com>
2023-12-12 23:01:56 +0000

    Inimigo a mexer-se

--- Script/Enemy/enemy.cs
+++ Script/Enemy/enemy.cs
@@ -5,7 +5,7 @@ public partial class enemy : CharacterBody2D
 {
 	public const float Speed = 150f;
 
-	Vector2 playerPosition = Vector2.Zero;
+	Vector2 playerPosition;
 	Vector2 mobPosition = Vector2.Zero;
 	Vector2 targetPosition = Vector2.Zero;
 	
@@ -13,23 +13,20 @@ public partial class enemy : CharacterBody2D
 	{
 		Vector2 velocity = Velocity;
 		mobPosition = this.Position;
-		playerPosition = GetNode<CharacterBody2D>("Player").Position;
-		targetPosition = (mobPosition - playerPosition).Normalized();
-		Velocity = targetPosition;
+		playerPosition = GetNode<CharacterBody2D>($"../Player").Position;
+		targetPosition = (playerPosition - mobPosition).Normalized();
 
-		targetPosition = targetPosition * Speed;
+		velocity = Vector2.Zero;
+
+		if (mobPosition.DistanceTo(playerPosition) < 100) {
+			velocity = targetPosition;
+		}
+
+		velocity = velocity * Speed;
 		Velocity = velocity;
+
 		MoveAndSlide();
 	}
-	
-	private void _on_player_detection_body_entered(Node2D body)
-	{
 
-	}
-	
-	private void _on_player_detection_body_exited(Node2D body)
-	{
-
-	}
 }
 

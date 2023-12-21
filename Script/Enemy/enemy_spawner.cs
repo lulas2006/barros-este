13457a012eb53181216296ddf531821295924db5
António Barros <abotinasbarros@gmail.com>
2023-12-21 17:49:15 +0000

    organização

---
+++ Script/Enemy/enemy_spawner.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class enemy_spawner : Node2D
+{
+	public Timer timer;
+	public PackedScene enemy2 = GD.Load<PackedScene>("res://enemy.tscn");
+
+	public override void _Ready()
+	{
+		timer = GetNode<Timer>("Timer");
+		timer.Start();
+	}
+
+	
+	private void _on_timer_timeout()
+	{
+		inst();
+	}
+
+	public void inst() {
+		var enemy1 = enemy2.Instantiate();
+		AddChild(enemy1);
+	}
+}
+
